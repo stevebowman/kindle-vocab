80326a15ab0877d1fdeddcd0e77b2b70e283e408
Steve Bowman <hullcitysteve@gmail.com>
2022-02-23 14:55:31 +0000

    Blank MAUI project

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace kindle_vocab;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace kindle_vocab;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
